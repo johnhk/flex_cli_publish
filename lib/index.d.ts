531324373b75e4fe01ea53e11955ad88a6f5ac4b
John Krogh <jhg@flexfone.dk>
2019-03-08 10:36:28 +0100

    testingCommit

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,19 +1,2 @@
 #!/usr/bin/env node
-declare let inquirer: any;
-declare const fs: any;
-declare let exec: any;
-declare const simpleGit: any;
-declare let env_config_paths: any;
-declare let readme_path: string;
-declare let env_config_var: any;
-declare let env_config_var_in: any;
-declare let all_answers: any;
-declare enum versiontype {
-    major = 0,
-    minor = 1,
-    patch = 2
-}
-declare function bump_package_version(vertype: versiontype): string;
-declare function update_readme(verstring: string): void;
-declare function update_env_files(verstring: string): void;
-declare function git_checkin(checkindata: any): void;
+export {};
--- lib/index.ts
+++ lib/index.ts
@@ -1,20 +1,23 @@
 #!/usr/bin/env node
 
-let inquirer = require("inquirer");
-const fs: any = require("fs-extra");
-let exec = require("child_process").execSync;
-const path = require("path");
-const simpleGit = require("simple-git")(__dirname);
-// var os = require("os");
-// import * as simplegit from 'simple-git/promise';
+import { execSync as exec } from "child_process";
+import * as fs from "fs-extra";
+import * as inquirer from "inquirer";
 import * as os from "os";
+import * as path from "path";
+import * as shell from "shelljs";
+import * as Console from "console";
+//import * as cmd from 'node-cmd';
+//import * as simplegit from "simple-git"
+var simplegit = require("simple-git");
+//import simplegit from "simple-git/promise";
 
-let env_config_paths: any = { config_in: "./enviroment.config.json", config_out: "./enviroment/enviroment.ts" };
-let readme_path: string = "./readme.md";
-let env_config_var: any;
-let env_config_var_in: any;
+const envConfigPaths: any = { config_in: "./enviroment.config.json", config_out: "./enviroment/enviroment.ts" };
+const readmePath: string = "./readme.md";
+let envConfigVar: any;
+let envConfigVarIn: any;
 
-let all_answers: any = [];
+let allAnswers: any = [];
 enum versiontype {
   major,
   minor,
@@ -22,150 +25,146 @@ enum versiontype {
 }
 
 
-/*
-inquirer
-  .prompt([
-    {
-      type: 'checkbox',
-      message: 'what changes',
-      name: 'what_changes',
-      choices: [
-        new inquirer.Separator(' = Select Changes = '),
-        {
-          name: 'Feature: A new feature'
-        },
-        {
-          name: 'Fix: A bug fix'
-        },
-        {
-          name: 'Doc: Documentation only changes'
-        },
-        {
-          name: 'Style: Cosmetic only changes'
-        },
-        {
-            name: 'Refacturing: Code changes, nether features nor fixes'
-        },
+function ask() {
+  inquirer
+    .prompt([
+      {
+        type: "checkbox",
+        message: "what changes",
+        name: "what_changes",
+        choices: [
+          new inquirer.Separator(" = Select Changes = "),
           {
-            name: 'Performance: Code changes that improves performance'
+            name: "Feature: A new feature"
           },
           {
-            name: 'Test: Adding missing tests'
+            name: "Fix: A bug fix"
           },
           {
-            name: 'Enviroment: Changes to the build process, auxiliary tools or libraries'
+            name: "Doc: Documentation only changes"
+          },
+          {
+            name: "Style: Cosmetic only changes"
+          },
+          {
+            name: "Refacturing: Code changes, nether features nor fixes"
+          },
+          {
+            name: "Performance: Code changes that improves performance"
+          },
+          {
+            name: "Test: Adding missing tests"
+          },
+          {
+            name: "Enviroment: Changes to the build process, auxiliary tools or libraries"
           }
-      ],
-      validate: function(answer: { length: number; }) {
-        if (answer.length < 1) {
-          return 'You must choose at least 1 change.';
-        }
+        ],
+        validate: function (answer: { length: number; }): any {
+          if (answer.length < 1) {
+            return "You must choose at least 1 change.";
+          }
 
-        return true;
-      }
-    },
-    {
-        type: 'confirm',
-        name: 'IsBreaking',
+          return true;
+        }
+      },
+      {
+        type: "confirm",
+        name: "IsBreaking",
         message: "Is this breaking changes?",
-        default: 'n',
+        default: "n",
         choices: [
           {
-            key: 'y',
-            name: 'yes',
-            value: 'yes'
+            key: "y",
+            name: "yes",
+            value: "yes"
           },
           {
-            key: 'n',
-            name: 'no',
-            value: 'no'
+            key: "n",
+            name: "no",
+            value: "no"
           }
         ]
-    }
-  ])
-  .then((answers: any) => {
-    all_answers.push(answers);
-    let prompt_array: any = [];
+      }
+    ])
+    .then((answers: any) => {
+      allAnswers.push(answers);
+      let prompt_array: any = [];
 
-    if(all_answers[0].IsBreaking == true) {
+      if (allAnswers[0].IsBreaking == true) {
+        prompt_array.push({
+          type: "input",
+          name: "breaking_info",
+          message: "Add bracking change description(s)"
+        });
+      }
+
+      allAnswers[0].what_changes.forEach(function (val: string): void {
+        let mystr: string = val.substr(0, 3);
+        switch (mystr) {
+          case "Fea":
+            prompt_array.push({
+              type: "input",
+              name: "feature_info",
+              message: "Add feature description(s)"
+            });
+            break;
+          case "Fix":
+            prompt_array.push({
+              type: "input",
+              name: "fix_info",
+              message: "Add fix description(s)"
+            });
+
+            break;
+          case "Env":
+            prompt_array.push({
+              type: "input",
+              name: "env_info",
+              message: "Add Enviromental change description(s)"
+            });
+            break;
+          default:
+          // code block
+        }
+      }
+      );
+
       prompt_array.push({
-        type: 'input',
-        name: 'breaking_info',
-        message: "Add bracking change description(s)"
+        type: "input",
+        name: "additional_info",
+        message: "Any additional info"
       });
-    }
 
-    all_answers[0].what_changes.forEach( function (val:string) {
-      let mystr = val.substr(0, 3);
-      switch(mystr) {
-        case 'Fea':
-          prompt_array.push({
-            type: 'input',
-            name: 'feature_info',
-            message: "Add feature description(s)"
-          });
-        break;
-        case 'Fix':
-          prompt_array.push({
-            type: 'input',
-            name: 'fix_info',
-            message: "Add fix description(s)"
-          });
 
-        break;
-        case 'Env':
-          prompt_array.push({
-            type: 'input',
-            name: 'env_info',
-            message: "Add Enviromental change description(s)"
-          });
-        break;
-        default:
-        // code block
-      }
-      }
-    )
+      inquirer
+        .prompt(prompt_array).then((next_answers: any) => {
+          allAnswers.push(next_answers);
 
-    prompt_array.push({
-      type: 'input',
-      name: 'additional_info',
-      message: "Any additional info"
+          run_updates(allAnswers);
+        });
     });
+}
 
-
-    inquirer
-      .prompt(prompt_array).then((next_answers: any) => {
-      all_answers.push(next_answers);
-
-      run_updates(all_answers);
-    })
-
-
-
-    //console.log(JSON.stringify(answers, null, '  '));
-  });
-*/
-
-function run_updates(all_answers: any) {
+function run_updates(allAnswers: any): void {
   let vtype: versiontype;
-  if (all_answers[0].IsBreaking == true) {
+  if (allAnswers[0].IsBreaking === true) {
     vtype = versiontype.major;
   } else {
-    if (all_answers[0].what_changes.includes("Feature: A new feature")) {
+    if (allAnswers[0].what_changes.includes("Feature: A new feature")) {
       vtype = versiontype.minor;
     } else {
       vtype = versiontype.patch;
     }
   }
 
-  let ver_with_v: string = bump_package_version(versiontype.patch);
-  let ver_without_v: string = ver_with_v.trim().replace("v", "");
-  let history = get_readme_formatted_text(all_answers, ver_without_v);
-  update_env_files(ver_without_v);
-  update_readme(ver_without_v, all_answers, history);
-  git_checkin(history, ver_without_v);
+  const verWithV: string = bump_package_version(versiontype.patch);
+  const verWithoutV: string = verWithV.trim().replace("v", "");
+  const history: string = get_readme_formatted_text(allAnswers, verWithoutV);
+  update_env_files(verWithoutV);
+  update_readme(verWithoutV, allAnswers, history);
+  git_checkin(history, verWithoutV);
 
-  console.log(JSON.stringify(all_answers, null, "  "));
+  console.log(JSON.stringify(allAnswers, null, "  "));
 }
 
 function bump_package_version(vertype: versiontype): string {
@@ -192,71 +191,84 @@ function bump_package_version(vertype: versiontype): string {
   return returnval.toString();
 }
 
-function get_readme_formatted_text(answers: any, ver: string): string {
+function get_readme_formatted_text(answers: any, _ver: string): string {
 
   let output: string = "";
-  for (var propt in answers[1]) {
-    switch (propt) {
-      case "breaking_info":
-        output += os.EOL + "\t\t" + "* [BREACKING CHANGE]" + os.EOL + "\t\t  " +  answers[1][propt];
-        break;
-      case "feature_info":
-        output += os.EOL + "\t\t" + "* [NEW FEATURES]" + os.EOL + "\t\t  " +  answers[1][propt];
-        break;
-      case "fix_info":
-        output += os.EOL + "\t\t" + "* [FIXES]" + os.EOL + "\t\t  " +  answers[1][propt];
-        break;
-      case "additional_info":
-        output += os.EOL + "\t\t" + "* [ADDITIONAL INFO]" + os.EOL + "\t\t  " +  answers[1][propt];
-        break;
-      default:
-      // code block
+  if (answers.length > 2) {
+    for (const propt in answers[1]) {
+      if (propt !== undefined) {
+        switch (propt) {
+          case "breaking_info":
+            output += os.EOL + "\t\t" + "* [BREACKING CHANGE]" + os.EOL + "\t\t  " + answers[1][propt];
+            break;
+          case "feature_info":
+            output += os.EOL + "\t\t" + "* [NEW FEATURES]" + os.EOL + "\t\t  " + answers[1][propt];
+            break;
+          case "fix_info":
+            output += os.EOL + "\t\t" + "* [FIXES]" + os.EOL + "\t\t  " + answers[1][propt];
+            break;
+          case "additional_info":
+            output += os.EOL + "\t\t" + "* [ADDITIONAL INFO]" + os.EOL + "\t\t  " + answers[1][propt];
+            break;
+          default:
+          // code block
+        }
+      }
     }
-    console.log(propt + ": " + answers[1][propt]);
   }
 
   return output;
 }
-function update_readme(verstring: string, answers: any, history: string) {
-  if (fs.existsSync(readme_path)) {
+function update_readme(verstring: string, _answers: any, history: string): void {
+  if (fs.existsSync(readmePath)) {
 
 
-    let readme_file: string = fs.readFileSync(readme_path);
+    let readme_file: any = fs.readFileSync(readmePath);
 
     readme_file = readme_file.toString().replace(/## Versioning[\n\r]*\d*\.\d*.\d*/i, "## Versioning" + os.EOL + os.EOL + verstring);
     // let history = get_readme_formatted_text(answers, verstring);
-    readme_file = readme_file.toString().replace(/## Release History[\n\r]*/i, "## Release History" + os.EOL + os.EOL + "* " + verstring + history + os.EOL);
+    readme_file = readme_file.toString().replace(/## Release History[\n\r]*/i, "## Release History" + os.EOL + os.EOL + "* " +
+                  verstring + history + os.EOL);
 
-    fs.outputFileSync(readme_path, readme_file, "utf8");
+    fs.outputFileSync(readmePath, readme_file, "utf8");
   }
 }
 
-function update_env_files(verstring: string) {
-  if (fs.existsSync(env_config_paths.config_in)) {
-    let env_config_file: Buffer = fs.readFileSync(env_config_paths.config_in);
-    env_config_var_in = JSON.parse(env_config_file.toString());
-    env_config_var_in.prod.prod.version = verstring;
-    env_config_var_in.prod.dev.version = verstring;
-    fs.outputFileSync(env_config_paths.config_in, JSON.stringify(env_config_var_in), "utf8");
+function update_env_files(verstring: string): void {
+  if (fs.existsSync(envConfigPaths.config_in)) {
+    let env_config_file: Buffer = fs.readFileSync(envConfigPaths.config_in);
+    envConfigVarIn = JSON.parse(env_config_file.toString());
+    envConfigVarIn.prod.prod.version = verstring;
+    envConfigVarIn.prod.dev.version = verstring;
+    fs.outputFileSync(envConfigPaths.config_in, JSON.stringify(envConfigVarIn), "utf8");
   }
-  if (fs.existsSync(env_config_paths.config_out)) {
-    let env_config_file: Buffer = fs.readFileSync(env_config_paths.config_out);
-    env_config_var_in = env_config_file.toString().replace(/"version":"\d*\.\d*.\d*"/i, "\"version\":\"" + verstring + "\"");
-    fs.outputFileSync(env_config_paths.config_out, env_config_var_in, "utf8");
+  if (fs.existsSync(envConfigPaths.config_out)) {
+    let env_config_file: Buffer = fs.readFileSync(envConfigPaths.config_out);
+    envConfigVarIn = env_config_file.toString().replace(/"version":"\d*\.\d*.\d*"/i, "\"version\":\"" + verstring + "\"");
+    fs.outputFileSync(envConfigPaths.config_out, envConfigVarIn, "utf8");
   }
 
 }
 
 function git_checkin(history: string, ver: string): void {
   // simpleGit.add("-A -- .");
-  var returnval = exec("git add -A -- .");
-  exec("git commit --quiet --message=\"asdfsadfasfd\" --all");
-  exec("git tag -a v1.4 -m \"my version 1.4\"");
-
-  // simpleGit.commit(history);
-  // simpleGit.addAnnotatedTag(ver, "version");
-
+  //exec("git add -A -- .");
+  //exec("git commit --quiet --message=\"" + history + "\" --all");
+  //exec("git tag -a " + ver + " -m \"version\"");
+  //let git: any = simplegit();
+  //git.commit(history);
+  //git.addAnnotatedTag(ver, "version");
+  let statusSummary = null;
+  try {
+     statusSummary = simplegit(__dirname).status();
+  }
+  catch (e) {
+     // handle the error
+  }
+console.log(statusSummary);
+  //git.status().then((status: StatusSummary) => { ... })
 }
+
 var my_answers: any = [
   {
     "what_changes": [
@@ -274,5 +286,26 @@ var my_answers: any = [
     "additional_info": "ewr er we rwe r"
   }
 ];
+
+function call_cli(): void {
+  //shell.cd("c:\\go");
+  //shell.exec('cd c:\\go');
+  //shell.exec('cd c:\\go', {silent:true}).stdout;
+  //Console.log('cd c:\\go');
+  //var process=cmd.get('node');
+
+  /*console.log(process.pid);*/
+  //cmd.run('cd ..');
+  //shell.exit(1);
+  //cmd.get(
+    //'cd ..',
+    /*function(err: any, data: any, _stderr: any){
+        console.log('the current dir contains these files :\n\n',data)
+    }*/
+  //);
+  process.stdout.write("cd ..");
+}
+
+//call_cli();
 
 run_updates(my_answers);
